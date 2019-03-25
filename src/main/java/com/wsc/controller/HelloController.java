eb5ac90112e605c7b2a9c2930ed97dee37f6ed34
lamda2019 <569188901@qq.com>
2019-03-25 22:46:49 +0800

    Initial commit

---
+++ src/main/java/com/wsc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.wsc.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","你好");
+        return "success";
+    }
+}
